448caefe86bcdfc0d3121ec792fb0e1cebf875c0
bridgenath1 <nathanbridgeearney@gmail.com>
2024-10-22 00:37:32 +1300

    Make paragraph holder fill space

--- components/Applications.vue
+++ components/Applications.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col bg-sky-100 dark:bg-sky-950">
     <h2 class="text-2xl font-bold p-4 bg-sky-200 dark:bg-sky-900 text-black dark:text-black">Applications</h2>
     <div class="flex-grow p-4 overflow-y-auto">
-      <div class="bg-white dark:bg-gray-600 shadow-md rounded-lg p-4">
+      <div class="h-full bg-white dark:bg-gray-600 shadow-md rounded-lg p-4">
         <h3 class="text-lg font-semibold text-black dark:text-black">{{ currentItem.title }}</h3>
         <div class="mt-2 text-black dark:text-black">
           <p v-for="(paragraph, index) in paragraphs" :key="index" class="mb-4">{{ paragraph }}</p>
